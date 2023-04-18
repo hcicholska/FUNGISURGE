bbf8a74cdc5564c1559b041357b7264c85d57c12
Hania <hcicholska@edu.cdv.pl>
2023-04-18 08:03:20 +0200

    pauza

--- Assets/PauseManager.cs
+++ Assets/PauseManager.cs
@@ -3,12 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class PauseManager : MonoBehaviour
 {
-
     public GameObject pauseMenu;
-
     private bool isPaused;
 
     void Start()
@@ -33,14 +30,37 @@ public class PauseManager : MonoBehaviour
         }
     }
 
+    public void ResumeGame()
+    {
+        // Twój kod do wznowienia gry
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+        isPaused = false;
+    }
+
+    public void PauseGame()
+    {
+        // Twój kod do zatrzymania gry
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+        isPaused = true;
+    }
+}
+
 public class ResumeButton : MonoBehaviour
 {
     public Button resumeButton;
     public PauseManager pauseManager;
-  
-        // Przypisujemy funkcję "ResumeGame" do zdarzenia "OnClick" przycisku
-        resumeButton.onClick.AddListener ResumeGame;
-   
-}
 
+    void Start()
+    {
+        // Przypisujemy funkcję "ResumeGame" do zdarzenia "OnClick" przycisku
+        resumeButton.onClick.AddListener(Resume);
+    }
+
+    void Resume()
+    {
+        // Wywołujemy metodę "ResumeGame" na obiekcie klasy "PauseManager"
+        pauseManager.ResumeGame();
+    }
 }
