1f891954d107871259008ca98363d2e9e11d836e
Hania <hcicholska@edu.cdv.pl>
2023-04-19 21:29:44 +0200

    kiedy hp=0, pokazuje się game over screen
    działa

--- Assets/health.cs
+++ Assets/health.cs
@@ -7,18 +7,23 @@ public class health : MonoBehaviour
 {
     public float healthPoints = 300f;
     public Slider healthSlider;
-
+public GameObject backgroundObject;
 
     private void Start()
     {
         healthSlider.maxValue = healthPoints;
+        backgroundObject.SetActive(false);
     }
 
     
     private void Update()
     {
         healthSlider.value = healthPoints;
-       
+       if (healthPoints <= 0 && !backgroundObject.activeSelf)
+        {
+            backgroundObject.SetActive(true);
+            Time.timeScale = 0f;
+        }
     }
 
 }
