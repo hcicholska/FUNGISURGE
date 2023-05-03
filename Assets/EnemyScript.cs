bdafdbdd8f2d3b1d266682113673e971ec4015d1
Hania <hcicholska@edu.cdv.pl>
2023-05-03 19:11:54 +0200

    podświetlanie na czerwono po uderzeniu enemy

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -3,12 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyScript : MonoBehaviour {
-    public int health = 40;
+    public int health = 100;
+    public float hitDuration = 0.5f;
+    private Color originalColor;
+    private Renderer renderer;
+
+    void Start() {
+        renderer = GetComponent<Renderer>();
+        originalColor = renderer.material.color;
+    }
 
     public void OnMouseDown() {
         health -= 10;
         if (health <= 0) {
             Destroy(gameObject);
         }
+        StartCoroutine(HitEffect());
     }
-} 
+
+    IEnumerator HitEffect() {
+        renderer.material.color = Color.red;
+        yield return new WaitForSeconds(hitDuration);
+        renderer.material.color = originalColor;
+    }
+}
