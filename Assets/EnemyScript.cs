7d0360c786e96cea89fa15b4bc3ddb2d9bf06727
Hania <hcicholska@edu.cdv.pl>
2023-05-03 19:05:21 +0200

    hania robi strzelanie

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+    public int health = 100;
+    public float hitDuration = 0.5f;
+    private Color originalColor;
+    private Renderer renderer;
+
+    void Start() {
+        renderer = GetComponent<Renderer>();
+        originalColor = renderer.material.color;
+    }
+
+    public void OnMouseDown() {
+        health -= 10;
+        if (health <= 0) {
+            Destroy(gameObject);
+        }
+        StartCoroutine(HitEffect());
+    }
+
+    IEnumerator HitEffect() {
+        renderer.material.color = Color.red;
+        yield return new WaitForSeconds(hitDuration);
+        renderer.material.color = originalColor;
+    }
+}
