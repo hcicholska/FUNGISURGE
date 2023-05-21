0c537f18ddd4ab7eeb5b6e0225969765dd6d29aa
MKraszewska <mkraszewska@edu.cdv.pl>
2023-05-21 22:02:44 +0200

    poprawka potek i amunicji

--- Assets/PlayerInventory.cs
+++ Assets/PlayerInventory.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class PlayerInventory : MonoBehaviour
 {
     public int NumberOfPotions { get; private set; }
+    public int NumberOfArrows { get; private set; }
 
     public void PotionCollected()
     {
         NumberOfPotions++;
     }
-  
+
+    public void ArrowCollected()
+    {
+        NumberOfArrows++;    
+    }
 }
